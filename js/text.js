57c63009170808e8b1cc37935fb58ec56dde25c9
2086556533 <m18335784129@163.com>
2020-08-10 08:07:15 +0800

    8.10

---
+++ js/text.js
@@ -0,0 +1,30 @@
+
+window.siteIsPc = true;
+window.foreignList = [];
+window.tenant = { "accessPool": "pool6", "appList": [], "attr": null, "bossProductInstance": "", "cdnFlag": "0", "ceYun": true, "createTime": { "date": 13, "day": 2, "hours": 9, "minutes": 33, "month": 7, "seconds": 21, "time": 1565660001000, "timezoneOffset": -480, "year": 119 }, "cusId": "", "domain": "www.wagsp.cn", "domainInfo": null, "domainStatus": 2, "domains": [], "foreign": false, "groupId": "", "hotelCusdomain": "", "hotelDomain": "", "id": 156508, "instanceList": [{ "appCodes": [], "bossProductInstance": "NEW2019081309410074152", "cusId": "2@tsrKqVt99WekENSYowRx", "flag": 1, "id": 223932, "insertDate": { "date": 13, "day": 2, "hours": 9, "minutes": 41, "month": 7, "seconds": 10, "time": 1565660470000, "timezoneOffset": -480, "year": 119 }, "memberId": "CEM10093342", "parent": null, "parentId": 0, "productDomain": "", "productionId": "100001", "producttype": 1, "tenant": null, "tenantApp": null, "tenantCode": "", "tenantId": 0, "tenants": [], "trade": "DOMESTIC_TRADE", "unittype": 0 }], "language": "zh_CN", "makeDomain": "1908135014.pool6-site.make.yun300.cn", "man": 1, "mobileDomain": "m.wagsp.cn", "mobileMakeDomain": "1908135014.pool6-msite.make.yun300.cn", "mobileProductDomain": "1908135014.pool6-msite.yun300.cn", "mobilePublishTime": { "date": 4, "day": 6, "hours": 9, "minutes": 15, "month": 0, "seconds": 26, "time": 1578100526000, "timezoneOffset": -480, "year": 120 }, "mobileStatus": 7, "mverify": "155840ea9a19f431047f8cc178e7079d", "mycat": "85", "name": "", "ningkerCusdomain": "", "ningkerDomain": "DOMESTIC_TRADE", "oganize": "[\"2\",\"1\"]", "pcPublishTime": { "date": 19, "day": 0, "hours": 16, "minutes": 21, "month": 0, "seconds": 43, "time": 1579422103000, "timezoneOffset": -480, "year": 120 }, "pool": "pool6", "productAttrId": 0, "productDomain": "1908135014.pool6-site.yun300.cn", "productInstance": { "appCodes": [], "bossProductInstance": "NEW2019081309410074152", "cusId": "2@tsrKqVt99WekENSYowRx", "flag": 1, "id": 223932, "insertDate": { "date": 13, "day": 2, "hours": 9, "minutes": 41, "month": 7, "seconds": 10, "time": 1565660470000, "timezoneOffset": -480, "year": 119 }, "memberId": "CEM10093342", "parent": null, "parentId": 0, "productDomain": "", "productionId": "100001", "producttype": 1, "tenant": null, "tenantApp": null, "tenantCode": "", "tenantId": 0, "tenants": [], "trade": "DOMESTIC_TRADE", "unittype": 0 }, "productInstanceCode": "NEW2019081309410074152", "productInstanceId": 223932, "publishPool": "", "rem": "888888", "resourcesPath": "/888888/pool6", "rusRoute": "pool6_888888", "status": 6, "targetPool": "", "targetRem": "", "templateCode": "core_whole_site", "tenantCode": "100001_1908135014", "tenantRelations": [], "tennatCode": "100001_1908135014", "unittype": "100001", "updateTime": { "date": 5, "day": 3, "hours": 9, "minutes": 50, "month": 1, "seconds": 50, "time": 1580867450000, "timezoneOffset": -480, "year": 120 }, "verify": "40220c7357cc92428f8763b74f789589", "youyiProductName": "" };
+window.commonShortUrl = ("http://www.ceurl.cn" == "") ? "" : "http://www.ceurl.cn" + "/";
+window.upgradeVersion = "ed7abb379630871692c727aa0870a1a1";
+var isxinnet = "false";
+window.noredirectCookieName = "_noredirect";
+var visittrack_siteId = "100001_1908135014";
+var visittrack_url = "";
+var gatherScripts = "";
+var unittype = window.tenant.unittype;
+window.globalObj = {};
+window.globalObj.isOpenSSL = false;
+try {
+    var setDomain = window.location.hostname.replace("http://", "").replace("https://", "");
+    if (setDomain.match(/[a-z]+/) != null) {
+        var domainArr = setDomain.split(".");
+        var preDomain = domainArr[domainArr.length - 2] + "."
+            + domainArr[domainArr.length - 1];
+        if (/(com|cn|org|net|xin|edu)\..*/.test(preDomain)) {
+            preDomain = domainArr[domainArr.length - 3] + "." + domainArr[domainArr.length - 2] + "."
+                + domainArr[domainArr.length - 1];
+        }
+        document.domain = preDomain;
+    }
+} catch (e) {
+    console.log(e);
+}
+
